d7a2e46a9c62b70f482f24b3190fe60506f90b9f
WaveDR <104482152+WaveDR@users.noreply.github.com>
2023-05-16 21:26:21 +0900

    [feat] Train Prefabs 구성
    1. 기차 모델링 프리팹 구현
    2. 파티클시스템으로 연기 구현

--- Assets/3.Script/Train/MoveTest.cs
+++ Assets/3.Script/Train/MoveTest.cs
@@ -6,16 +6,26 @@ using UnityEngine.AI;
 
 public class MoveTest : MonoBehaviour
 {
-    [SerializeField] private NavMeshAgent nav;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private NavMeshAgent nav;
 
+    [SerializeField] private Transform target;
+    public float curSpeed;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        nav = GetComponent<NavMeshAgent>();
+    }
     // Update is called once per frame
     void Update()
     {
+        curSpeed = nav.speed;
+        nav.SetDestination(target.position);
+
+        if(nav.speed == 0)
+        {
+            Debug.Log("Stop");
+        }
+
         
     }
 }
