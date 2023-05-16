7027486a4a28083296b8a67384c9ec3601d42d91
WaveDR <104482152+WaveDR@users.noreply.github.com>
2023-05-16 21:46:54 +0900

    [feat] singleton Handler 구현
    1. 싱글톤 상속을 받을 수 있는 Singleton handler Script를 ETC폴더에 구현해놨습니다.

--- Assets/3.Script/Train/MoveTest.cs
+++ Assets/3.Script/Train/MoveTest.cs
@@ -1,31 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 
 
-public class MoveTest : MonoBehaviour
+public class MoveTest : Singleton<MoveTest>
 {
-    private NavMeshAgent nav;
-
-    [SerializeField] private Transform target;
+    public Queue<Transform> nextTarget = new Queue<Transform>();
     public float curSpeed;
     // Start is called before the first frame update
     private void Awake()
     {
-        nav = GetComponent<NavMeshAgent>();
+
     }
     // Update is called once per frame
     void Update()
     {
-        curSpeed = nav.speed;
-        nav.SetDestination(target.position);
-
-        if(nav.speed == 0)
-        {
-            Debug.Log("Stop");
-        }
-
         
+    }
+
+    public void EnqueueRailPos(Transform transform)
+    {
+        nextTarget.Enqueue(transform);
     }
 }
