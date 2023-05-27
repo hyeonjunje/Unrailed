a78e293b13835219d41b370fdd2abc0db07f760e
hyeonjunje <88482159+hyeonjunje@users.noreply.github.com>
2023-05-27 15:46:22 +0900

    20230527 저장본
    코루틴때문에
    플로우가 섞여서 높이 반영이 안됨...

---
+++ Assets/3.Script/MapEditor/MapCreator.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapCreator : MonoBehaviour
+{
+    [SerializeField] private BlockMK2 _blockPrefab;
+    [SerializeField] private Material[] _blocksMaterial;
+    [SerializeField] private Transform _blockParent;
+    [SerializeField] private ItemPrefabData _itemPrefabData;
+    [SerializeField] private BlockTransformerData[] _blockTransformerData;
+    [SerializeField] private Transform _waterFallUp;
+    [SerializeField] private Transform _waterFallDown;
+
+    // 맵은 순서대로 생성하기로 하자
+    // 여기서 거리 UI 만들어??
+
+    private List<List<BlockMK2>> groundList;
+
+    public IEnumerator CreateMapCo(int worldIndex)
+    {
+        // 비동기가 필요하긴 할 듯
+        MapData mapData = FileManager.MapsData.mapsData[worldIndex];
+        LoadMap(mapData);
+
+        yield return null;
+
+        SetHeightMap();
+
+        yield return new WaitForEndOfFrame();
+    }
+
+    private void LoadMap(MapData mapData)
+    {
+        int x = mapData.mapData[0].arr.Length;
+        int y = mapData.mapData.Length;
+
+        int minX = (x / 2) - 1;
+        int minY = (y / 2) - 1;
+
+        groundList = new List<List<BlockMK2>>();
+
+        for(int i = 0; i < y; i++)
+        {
+            groundList.Add(new List<BlockMK2>());
+            for(int j = 0; j < x; j++)
+            {
+                BlockMK2 go = Instantiate(_blockPrefab, _blockParent);
+                go.transform.localPosition = new Vector3(j - minX, 0, i - minY);
+                groundList[i].Add(go);
+            }
+        }
+
+        for(int i = 0; i < y; i++)
+        {
+            for(int j = 0; j < x; j++)
+            {
+                int index = mapData.mapData[i].arr[j];
+                InitBlock(groundList[i][j], index);
+            }
+        }
+    }
+
+    private void InitBlock(BlockMK2 block, int index)
+    {
+        BlockTransformerData blockTransformerData = null;
+        if (index == (int)EBlock.iron)
+            blockTransformerData = _blockTransformerData[0];
+        else if (index == (int)EBlock.blackRock)
+            blockTransformerData = _blockTransformerData[1];
+
+        Material blockMaterial = null;
+
+        if (index > (int)EBlock.blackRock)
+            blockMaterial = _blocksMaterial[(int)EBlock.grass];
+        else
+            blockMaterial = _blocksMaterial[index];
+
+        StartCoroutine(InitBlockCo(block, blockMaterial, index, blockTransformerData));
+    }
+
+    // 한 프레임 후에 실행해야 제대로 작동함
+    private IEnumerator InitBlockCo(BlockMK2 block, Material blockMaterial, int index, BlockTransformerData blockTransformerData)
+    {
+        yield return null;
+        block.Init(index, blockMaterial, _itemPrefabData.itemPrefabs[index], blockTransformerData);
+
+        if (index == (int)EBlock.water)
+        {
+            // 위쪽이면 _waterFallUp, 밑쪽이면 _waterFallDown 생성
+            if (block.transform.localPosition.z == -9)
+                Instantiate(_waterFallDown.gameObject, block.transform);
+            else if (block.transform.localPosition.z == groundList.Count - 10)
+                Instantiate(_waterFallUp.gameObject, block.transform);
+        }
+    }
+
+    private void SetHeightMap()
+    {
+        int x = groundList[0].Count;
+        int y = groundList.Count;
+
+        for (int i = 0; i < y; i++)
+        {
+            for (int j = 0; j < x; j++)
+            {
+                if (groundList[i][j].Index == (int)EBlock.blackRock)
+                {
+                    int height = groundList[i][j].GetHeight();
+                    Transform child = groundList[i][j].transform.GetChild(0);
+                    child.localScale = Vector3.one + Vector3.up * height * 0.3f;
+                    Debug.Log(child.localScale);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/3.Script/Scene/InGameScene.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameScene : MonoBehaviour
+{
+    [SerializeField] private GameObject _loadingSceneUI;
+
+    [SerializeField] private MapCreator _mapCreator;
+
+    private void Awake()
+    {
+        FileManager.LoadGame();
+
+        // 로딩 씬 보여주기
+        _loadingSceneUI.SetActive(true);
+
+        // 보여줄동안 
+        // 플레이어 생성
+        // 맵 생성
+        // 동물, 도적, ai 생성
+        // 긴 맵을 만들고 자르기??
+        // astar 맵 최신화
+        // StartCoroutine(LodingCo());
+        Debug.Log(Time.realtimeSinceStartup);
+
+        // 이 작업 다 끝나고 로딩 씬 풀고
+        // 자른 맵 내려오면서 맵 조립
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+            StartCoroutine(LodingCo());
+    }
+
+    private IEnumerator LodingCo()
+    {
+        // 맵 생성
+        // 플레이어 생성
+        // 동물, 도적, ai 생성
+        yield return StartCoroutine(_mapCreator.CreateMapCo(0));
+        Debug.Log(Time.realtimeSinceStartup);
+
+        _loadingSceneUI.SetActive(false);
+
+    }
+}
