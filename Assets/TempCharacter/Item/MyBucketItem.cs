a5584820c0cf5773bb98c2ff60c0665a5575ef12
hyeonjunje <88482159+hyeonjunje@users.noreply.github.com>
2023-06-03 15:11:35 +0900

    [refactor] 플레이어 석환민경 합침

--- Assets/TempCharacter/Item/MyBucketItem.cs
+++ Assets/TempCharacter/Item/MyBucketItem.cs
@@ -2,8 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MyNonStackableItem : MyItem
+public class MyBucketItem : MyItem
 {
+    [SerializeField] private GameObject _waterMesh;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        _waterMesh.SetActive(false);
+    }
+
     public override Pair<Stack<MyItem>, Stack<MyItem>> AutoGain(Stack<MyItem> handItem, Stack<MyItem> detectedItem)
     {
         // 이건 아무것도 안해도 돼
@@ -15,7 +23,7 @@ public class MyNonStackableItem : MyItem
         // handItem은 무조건 nonStackableItem
 
         EItemType detectedItemType = detectedItem.Peek().ItemType;
-        if(detectedItemType == EItemType.axe || detectedItemType == EItemType.pick || detectedItemType == EItemType.bucket)
+        if (detectedItemType == EItemType.axe || detectedItemType == EItemType.pick || detectedItemType == EItemType.bucket)
         {
             MyItem temp = handItem.Pop();
             handItem.Push(detectedItem.Pop());
@@ -23,10 +31,10 @@ public class MyNonStackableItem : MyItem
             handItem.Peek().RePosition(handItem.Peek().equipment, Vector3.zero);
             detectedItem.Peek().RePosition(player.AroundEmptyBlockTranform, Vector3.up * 0.5f);
         }
-        else if(detectedItemType == EItemType.wood || detectedItemType == EItemType.steel)
+        else if (detectedItemType == EItemType.wood || detectedItemType == EItemType.steel)
         {
             MyItem temp = handItem.Pop();
-            for(int i = 0; i < 3; i++)
+            for (int i = 0; i < 3; i++)
             {
                 if (detectedItem.Count == 0)
                     break;
@@ -37,7 +45,7 @@ public class MyNonStackableItem : MyItem
             detectedItem.Push(temp);
             detectedItem.Peek().RePosition(player.AroundEmptyBlockTranform, Vector3.up * 0.5f);
         }
-        else if(detectedItemType == EItemType.rail)
+        else if (detectedItemType == EItemType.rail)
         {
             if (IsRailInteractive(detectedItem.Peek().GetComponent<RailController>()))
             {
@@ -72,5 +80,10 @@ public class MyNonStackableItem : MyItem
         detectedItem.Push(handItem.Pop());
         detectedItem.Peek().RePosition(player.AroundEmptyBlockTranform, Vector3.up * 0.5f);
         return new Pair<Stack<MyItem>, Stack<MyItem>>(handItem, detectedItem);
+    }
+
+    public override void ActiveWater(bool flag)
+    {
+        _waterMesh.SetActive(flag);
     }
 }
